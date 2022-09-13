eba0d94332ee977c9261716ffe6349bfcb6ab64f
seif <baazouziseif9@gmail.com>
2022-09-13 09:23:52 +0100

    add 404 page

--- src/App.svelte
+++ src/App.svelte
@@ -5,12 +5,15 @@
 	import Login from "./pages/login.svelte";
 	import Signup from "./pages/signup.svelte";
 	import Workspace from "./pages/workspace.svelte";
+	import NotFound from "./pages/404.svelte";
 
 	const routes = {
 		"/": Home,
 		"/login": Login,
 		"/signup": Signup,
 		"/workspace/:workspaceID": Workspace,
+
+		"*": NotFound,
 	};
 </script>
 
---
+++ src/pages/404.svelte
@@ -0,0 +1,8 @@
+<script>
+    import DefaultLayout from "~/layouts/DefaultLayout.svelte";
+    import Error from "~/components/Error.svelte";
+</script>
+
+<DefaultLayout>
+    <Error title="404 Error" message="Page not found." />
+</DefaultLayout>
